41d4f8e459ca3cd65f910e01fda3560a80df4283
Leschinsky Oleg <helge9210@gmail.com>
2017-09-21 22:01:47 +0300

    Implementing "http://web.cecs.pdx.edu/~bart/cs510cs/papers/korf-ckk.pdf". Fails on MemoryError

--- hard/minimumsumpartition.py
+++ hard/minimumsumpartition.py
@@ -6,26 +6,39 @@ http://practice.geeksforgeeks.org/problems/minimum-sum-partition/0
 """
 
 from pprint import pprint
+import heapq
+from collections import deque
 
 def find_partition(array):
-    s = sum(array)
-    p = [[True for _ in range(len(array) + 1)]]
-    for _ in range(s):
-        row = []
-        for _ in range(len(array) + 1):
-            row.append(False)
-        p.append(row)
-    print(s)
-    for i in range(s):
-        for j in range(len(array)):
-            if i - array[j - 1] >= 0:
-                p[i][j] = p[i][j-1] or p[i-array[j-1]][j-1]
-            else:
-                p[i][j] = p[i][j-1]
+    rev_array = [-1*el for el in array]
+    heapq.heapify(rev_array)
+    queue = deque([rev_array[:]])
+    min_diff = sum(array)
+    while len(queue) > 0:
+        r = queue.popleft()
+        #print(r)
+        #print(min_diff)
+        if len(r) == 1:
+            continue
+        if min_diff == 1:
+            break
+        if -1*r[0] > -1*sum(r[1:]):
+            min_diff = min(min_diff, -1*r[0] - -1*sum(r[1:]))
+            continue
+        elif r[0] == sum(r[1:]):
+            min_diff = 0
+            break
+        else:
+            n1 = -1*heapq.heappop(r)
+            n2 = -1*heapq.heappop(r)
+            queue.append(r[:])
+            #print('right', n2 + n1)
+            heapq.heappush(queue[-1], -1*(n2 + n1))
+            queue.append(r[:])
+            #print('left', n1 - n2)
+            heapq.heappush(queue[-1], -1*(n1 - n2))
 
-    pprint(p)
-    print(p[int(s/2)][len(array)])
-
+    return min_diff
 
 
 t = int(input())
