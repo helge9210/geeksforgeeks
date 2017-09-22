c496982fb072103d3576bc8a7732bf493ea54748
Leschinsky Oleg <helge9210@gmail.com>
2017-09-22 10:48:30 +0300

    Removal of unused code in mimimum array partitioning

--- hard/minimumsumpartition.py
+++ hard/minimumsumpartition.py
@@ -5,9 +5,8 @@
 http://practice.geeksforgeeks.org/problems/minimum-sum-partition/0
 """
 
-from pprint import pprint
 import heapq
-from collections import deque
+
 
 def find_partition(array):
     rev_array = [-1*el for el in array]
@@ -16,8 +15,6 @@ def find_partition(array):
     min_diff = sum(array)
     while len(queue) > 0:
         r = queue.popleft()
-        #print(r)
-        #print(min_diff)
         if len(r) == 1:
             continue
         if min_diff == 1:
@@ -32,10 +29,8 @@ def find_partition(array):
             n1 = -1*heapq.heappop(r)
             n2 = -1*heapq.heappop(r)
             queue.append(r[:])
-            #print('right', n2 + n1)
             heapq.heappush(queue[-1], -1*(n2 + n1))
             queue.append(r[:])
-            #print('left', n1 - n2)
             heapq.heappush(queue[-1], -1*(n1 - n2))
 
     return min_diff
