1e3c306c82e4febf304a680dcd1291afe896c3b8
Leschinsky Oleg <helge9210@gmail.com>
2017-09-23 11:08:06 +0300

    Minimal points to reach destination. Dynamic programming solution

--- hard/minimumpointstoreachdestination.py
+++ hard/minimumpointstoreachdestination.py
@@ -6,7 +6,6 @@ http://practice.geeksforgeeks.org/problems/minimum-points-to-reach-destination/0
 """
 
 
-from pprint import pprint
 from collections import deque
 
 
@@ -34,9 +33,9 @@ def get_next_steps(array, row, col):
     steps = []
     rows = len(array)
     cols = len(array[0])
-    directions = [(1, 0),  # down
+    directions = [(-1, 0),  # up
 
-                  (0, 1)]  # right
+                  (0, -1)]  # left
     for direction in directions:
         if row + direction[0] >= 0 and \
            row + direction[0] < rows and \
@@ -47,31 +46,47 @@ def get_next_steps(array, row, col):
     return steps
 
 
+def get_neighbours(array, row, col):
+    costs = []
+    rows = len(array)
+    cols = len(array[0])
+    directions = [(1, 0),  # down
+
+                  (0, 1)]  # right
+    for direction in directions:
+        if row + direction[0] >= 0 and \
+           row + direction[0] < rows and \
+           col + direction[1] >= 0 and \
+           col + direction[1] < cols:
+            costs.append(array[row+direction[0]][col+direction[1]])
+    return costs
+
+
 def find_min_path(array, rows, cols):
     maze = convert_array(array, rows, cols)
-    print_maze(maze)
-    stack = [((0, 0), [])]
-    pathes = []
+    costs = create_maze(rows, cols)
+    # print_maze(maze)
+    # print_maze(costs)
+    stack = deque()
+    stack.append((rows - 1, cols - 1))
     while stack:
-        current = stack.pop()
-        if current[0] == (rows - 1, cols - 1):
-            v = current[1][:]
-            v.append(current[0])
-            p = []
-            for el in v:
-                p.append(maze[el[0]][el[1]])
-            pathes.append(p)
-            continue
-        if current[0] not in current[1]:
-            v = current[1][:]
-            v.append(current[0])
-            next_steps = get_next_steps(maze, current[0][0],
-                                        current[0][1])
-            for n in next_steps:
-                stack.append((n, v[:]))
+        current = stack.popleft()
+        next_steps = get_next_steps(maze, current[0], current[1])
+        for step in next_steps:
+            stack.append(step)
+        nearby_costs = get_neighbours(costs, current[0], current[1])
+        if len(nearby_costs) == 0:
+            costs[current[0]][current[1]] = max(
+                1 - maze[current[0]][current[1]], 1)
+        else:
+            c = min([max(v - maze[current[0]][current[1]], 1)
+                     for v in nearby_costs if v is not None])
+            costs[current[0]][current[1]] = c
+        if current == (0, 0):
+            break
+    # print_maze(costs)
 
-    pprint(pathes) # calculate minimal points per path, chose min
-
+    return costs[0][0]
 
 
 t = int(input())
@@ -79,5 +94,3 @@ for i in range(0, t):
     r, c = [int(v) for v in input().strip().split()]
     arr = [int(v) for v in input().strip().split()]
     print(find_min_path(arr, r, c))
-
-
