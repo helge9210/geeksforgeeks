ded654ec74c8bda24ea4e46a755d7e9593d797c0
Leschinsky Oleg <helge9210@gmail.com>
2017-09-23 13:55:58 +0300

    Minimal points to reach destination. Accepted solution. 1-D array

--- hard/minimumpointstoreachdestination.py
+++ hard/minimumpointstoreachdestination.py
@@ -15,26 +15,9 @@ def print_maze(maze):
         print(row)
 
 
-def convert_array(array, rows, cols):
-    output = []
-    for row in range(rows):
-        output.append(array[row*rows:row*rows+cols])
-    return output
-
-
-def create_maze(rows, cols):
-    output = []
-    for row in range(rows):
-        output.append([None]*cols)
-    return output
-
-
-def get_next_steps(array, row, col):
+def get_next_steps(array, row, col, rows, cols):
     steps = []
-    rows = len(array)
-    cols = len(array[0])
     directions = [(-1, 0),  # up
-
                   (0, -1)]  # left
     for direction in directions:
         if row + direction[0] >= 0 and \
@@ -46,47 +29,46 @@ def get_next_steps(array, row, col):
     return steps
 
 
-def get_neighbours(array, row, col):
+def get_neighbours(array, row, col, rows, cols):
     costs = []
-    rows = len(array)
-    cols = len(array[0])
     directions = [(1, 0),  # down
-
                   (0, 1)]  # right
     for direction in directions:
         if row + direction[0] >= 0 and \
            row + direction[0] < rows and \
            col + direction[1] >= 0 and \
            col + direction[1] < cols:
-            costs.append(array[row+direction[0]][col+direction[1]])
+            costs.append(array[(row+direction[0])*cols
+                               + col+direction[1]])
     return costs
 
 
 def find_min_path(array, rows, cols):
-    maze = convert_array(array, rows, cols)
-    costs = create_maze(rows, cols)
+    costs = [None]*rows*cols
     # print_maze(maze)
     # print_maze(costs)
     stack = deque()
     stack.append((rows - 1, cols - 1))
     while stack:
         current = stack.popleft()
-        next_steps = get_next_steps(maze, current[0], current[1])
+        next_steps = get_next_steps(array, current[0], current[1],
+                                    rows, cols)
         for step in next_steps:
             stack.append(step)
-        nearby_costs = get_neighbours(costs, current[0], current[1])
+        nearby_costs = get_neighbours(costs, current[0], current[1],
+                                      rows, cols)
         if len(nearby_costs) == 0:
-            costs[current[0]][current[1]] = max(
-                1 - maze[current[0]][current[1]], 1)
+            costs[current[0]*cols + current[1]] = max(
+                1 - array[current[0]*cols + current[1]], 1)
         else:
-            c = min([max(v - maze[current[0]][current[1]], 1)
+            c = min([max(v - array[current[0]*cols + current[1]], 1)
                      for v in nearby_costs if v is not None])
-            costs[current[0]][current[1]] = c
+            costs[current[0]*cols + current[1]] = c
         if current == (0, 0):
             break
     # print_maze(costs)
 
-    return costs[0][0]
+    return costs[0]
 
 
 t = int(input())
